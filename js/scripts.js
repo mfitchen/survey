1dd0668f02730e62e46727bf802942177409cdeb
Martin Fitchen <mfitchen@Martins-iMac.local>
2016-06-01 21:05:03 -0700

    add the paragraphs after the survey is submitted utilizing jQuery and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    event.preventDefault();
+
     var name = $("#name").val();
     var dob = $("#born").val();
     var favoriteColor = $("#color").val();
     var beverage = $("#beverage").val();
     var flavor = $("input:radio[name=flavor]:checked").val();
+    var music = $("#music").val();
+
+    $(".name").text(name);
+    $(".born").text(dob);
+    $(".color").text(favoriteColor);
+    $(".beverage").text(beverage);
+    $(".flavor").text(flavor);
+    $(".music").text(music);
 
 //  var result = $("Your name is " + name + " and you like " + flavor + " flavored ice cream.")
 //  $("#results").text(result);
 
     $("#results").show();
+
+    event.preventDefault();
   });
 });
